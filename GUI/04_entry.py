019179e18f95352e43c72603739482bf8cda9954
soma0sd <soma0sd@gmail.com>
2016-09-07 15:17:58 +0900

    coments update

--- GUI/04_entry.py
+++ GUI/04_entry.py
@@ -12,12 +12,16 @@ from numpy import random
 
 
 def random_walk():
+    """
+    랜덤워크 함수.
+    각 요소들이 x 방향으로 2씩 전진하면서 y축으로는 랜덤하게 움직이도록 만드는 함수
+    """
     global run, obj, can, pos, result
     while run:
         pos += 2
-        if pos >= 299:
+        if pos >= 299:  # 화면 끝에 도달하면 정지
             pos = 0
-            for o in obj:
+            for o in obj:  # 도달한 오브젝트들의 y값을 도수분포표로 출력하는 루프
                 x = can.coords(o)[1]+1
                 for i in range(10):
                     if x > (i+1)*20:
@@ -27,12 +31,16 @@ def random_walk():
                         break
             print(result)
             break
-        for o in obj:
+        for o in obj:  # 각요소가 [dx, yy] 만큼 움직이도록 루프
             can.move(o, 2, random.randint(5)-2)
-        can.update()
+        can.update()  # 캔버스 업데이트
 
 
 def go():
+    """
+    애니메이션 초기화+실행 함수
+    일시정지상태일 경우 남은 이동을 수행한다.
+    """
     global obj, can, run, pos, member
     lim = int(member.get())
     if pos == 0:
@@ -42,9 +50,17 @@ def go():
 
 
 def stop():
+    """
+    일시정지 함수. random_walk의 while루프에 False를 줘서 임의로 정지시킴
+    """
     global run, can
     run = False
 
+
+"""
+본문
+2개의 프레임을 선언해서 캔버스와 메뉴 영역을 나눔
+"""
 result = [0]*10
 pos = 0
 master = Tk()
@@ -59,11 +75,20 @@ frame_menu.pack()
 can = Canvas(frame_canv, width=300, height=200)
 can.pack()
 
+"""
+엔트리 요소
+직접 타이핑을 통해 프로그램 안에서 변수를 통제
+"""
 member = StringVar()
 member.set(100)
 entry = Entry(frame_menu, textvariable=member)
 entry.grid(row=0, column=1, columnspan=2, sticky=W)
+# columnspan: 열1과 열2에 걸쳐서 레이아웃
 
+"""
+버튼
+[go]와 [stop]버튼을 배치
+"""
 btn_go = Button(frame_menu, text="go", command=go)
 btn_stop = Button(frame_menu, text="stop", command=stop)
 btn_go.grid(row=1, column=1, sticky=W+E)
--- GUI/05_drag_object.py
+++ GUI/05_drag_object.py
@@ -12,6 +12,10 @@ import numpy as np
 
 
 def onClick(e):
+    """
+    클릭했을 때 원을 클릭했는지 배경을 클릭했는지를 판단.
+    이후 드래그에 따라 요소가 움직일지 말지를 결정
+    """
     global obj, can, onTaget
     minx = can.coords(obj)[0]
     miny = can.coords(obj)[1]
@@ -24,6 +28,9 @@ def onClick(e):
 
 
 def onDrag(e):
+    """
+    만약 원을 클릭했을 때, 드래그하는 동안 원이 마우스를 따라가도록 하는 함수
+    """
     global obj, can, onTaget
     if not onTaget:
         return None
@@ -31,6 +38,10 @@ def onDrag(e):
 
 
 def web():
+    """
+    원의 위치에 따라 영향받는 주변 환경을 표현
+    역제곱 규칙에 따름
+    """
     global can, linex, liney, obj
     ratio = 500
     while True:
@@ -49,20 +60,30 @@ def web():
                 posy += (x, y)
             can.delete(linex[int(y/50)])
             can.delete(liney[int(y/50)])
-            linex[int(y/50)] = [can.create_line(posx, fill='#f00', dash=(3, 2))]
+            linex[int(y/50)] = [can.create_line(posx, fill='#f00',
+                                dash=(3, 2))]
             liney[int(y/50)] = [can.create_line(posy, fill='#000')]
         can.update()
 
+"""
+본문
+캔버스를 선언하고 원과 선들을 초기화
+"""
 master = Tk()
 can = Canvas(master, width=500, height=500)
 obj = can.create_oval(240, 240, 260, 260, fill='#aafcdf')
 can.pack()
 
+# 라인 초기화
 linex = [can.create_line(0, 0, 1, 1, fill='#f00', dash=(3, 2))]*11
 liney = [can.create_line(0, 0, 1, 1, fill='#f00', dash=(3, 2))]*11
 
+# 애니메이션 구동
 can.after(0, web)
 onTaget = False
+
+# bind: 미리 정해져 있는 입력에 따라서 함수를 실행
+# 사용자의 드래그 동작은 클릭과 클릭한 후 이동으로 구성되어 있다.
 can.bind('<Button-1>', onClick)
 can.bind('<B1-Motion>', onDrag)
 
