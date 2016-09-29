3fc550efcb9d16842e52091b8be7650e66fcbe71
soma0sd <soma0sd@gmail.com>
2016-09-29 23:13:21 +0900

    update

--- W4-Draw_electro/draw_stream.py
+++ W4-Draw_electro/draw_stream.py
@@ -27,18 +27,38 @@ class SingleCharge:
     def is_crush(self, x, y):
         ix = x - self.x
         iy = y - self.y
-        if self.opt['r']**2 < ix**2+iy**2:
+        if self.opt['r']**2 > ix**2+iy**2:
             return True
         else:
             return False
 
+    def clone(self, **kw):
+        option = {'x': np.random.randint(30, 470),
+                  'y': np.random.randint(80, 470)}
+        option.update(kw)
+        return SingleCharge(self.canvas, option['x'],  option['y'],
+                            self.charge)
 
-class AddMenu:
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+        r = self.opt['r']
+        self.canvas.coords(self.oval, x-r, y-r, x+r, y+r)
+        self.canvas.coords(self.text, x, y)
+
+    def delete(self):
+        self.canvas.delete(self.oval)
+        self.canvas.delete(self.text)
+
+
+class control:
     def __init__(self, canvas, **kw):
         self.opt = {'w': 500, 'h': 50, 'maxima': 5}
         self.opt.update(kw)
         self.canvas = canvas
         self.btn = []
+        self.object = []
+        self._flag = None
         # Menu Draw
         maxi = self.opt['maxima']
         width = self.opt['w']
@@ -46,11 +66,36 @@ class AddMenu:
         for i in range(maxi*2+1):
             if i == maxi:
                 continue
-            SingleCharge(canvas, (i+1)*width/(maxi*2+2), cy, i-maxi)
+            bt = SingleCharge(canvas, (i+1)*width/(maxi*2+2), cy, i-maxi)
+            self.btn.append(bt)
+        canvas.bind('<Button-1>', self.B1Action)
+        canvas.bind('<B1-Motion>', self.B1Motion)
+
+    def B1Action(self, event):
+        # Find active button
+        self._flag = None
+        for b in self.btn:
+            if b.is_crush(event.x, event.y):
+                self.object.append(b.clone())
+        for o in self.object:
+            if o.is_crush(event.x, event.y):
+                self._flag = o
+
+    def B1Motion(self, event):
+        if self._flag is None:
+            return None
+        self._flag.move(event.x, event.y)
+        if event.y < 50:
+            self.delete_object(self._flag)
+
+    def delete_object(self, obj):
+        obj.delete()
+        del self.object[self.object.index(obj)]
+        return None
 
 
 master = tk.Tk()
 canvas = tk.Canvas(master, width=500, height=500, bg='#000')
 canvas.pack()
-AddMenu(canvas)
+control(canvas)
 master.mainloop()
