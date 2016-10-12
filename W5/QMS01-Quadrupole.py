71ecc2ab51dac89d6b48a68b78ce245d08877d45
DESKTOP-QANPK0N\soma0sd <soma0sd@gmail.com>
2016-10-13 04:36:37 +0900

    코멘트 추가

--- W5/QMS01-Quadrupole.py
+++ W5/QMS01-Quadrupole.py
@@ -9,27 +9,39 @@ import time
 
 
 class QMS(tk.Canvas):
+    """
+    평면에서 이중극으로 변환해서 시뮬레이션 하는 프로그램
+    """
     def __init__(self, *arg, **kw):
         tk.Canvas.__init__(self, *arg, **kw)
-        self.vx = 180
+        self.vx = 180  # 가로방향 속도
         self.pack()
         self._create_rods()
-        self.after(0, self.animations)
-        self.rod_voltage = False
-        self.ions = []
+        self.after(0, self.animations)  # 에니메이션 작동
+        self.rod_voltage = False  # 전극 작동 스위치
+        self.ions = []  # 이온 리스트
 
     def _create_rods(self):
+        """
+        캔버스 초기화, 실험실 Draw
+        """
         w, h = self.winfo_reqwidth(), self.winfo_reqheight()
         N = self.create_rectangle(0, 0, w, 30, fill='#000')
         S = self.create_rectangle(0, h, w, h-30, fill='#000')
         self.rods = {'N': N, 'S': S, 'V': 0}
 
     def rod_on(self):
+        """
+        전극 스위치 True는 False로 False는 True로
+        """
         self.rod_voltage = not self.rod_voltage
-        if not self.rod_voltage:
+        if not self.rod_voltage:  # 만약 스위치가 꺼져있다
             self.rods['V'] = 0
 
     def create_ion(self):
+        """
+        이온생성
+        """
         h = self.winfo_reqheight()/2+2
         ind = self.create_oval(0, h-5, 10, h+5, fill='#F00')
         chg = np.random.randint(1, 4)
@@ -37,6 +49,9 @@ class QMS(tk.Canvas):
         self.ions.append({'Oid': ind, 'Tid': tid, 'chg': chg, 'vy': 0, 'm': 1})
 
     def move_ion(self, ion, rate):
+        """
+        이온 이동 함수
+        """
         w, h = self.winfo_reqwidth(), self.winfo_reqheight()
         pos = self.coords(ion['Tid'])
         Fn = 8.99E9*1.602E-7*self.rods['V']*ion['chg']/(pos[1]-30)**2
@@ -51,10 +66,16 @@ class QMS(tk.Canvas):
             del self.ions[ind]
 
     def attachs(self, f, v):
+        """
+        외부 슬라이드바와 실시간 연결
+        """
         self.Freq = f
         self.Volt = v
 
     def animations(self):
+        """
+        에니메이션
+        """
         t0 = time.time()
         while True:
             td = time.time()-t0
@@ -82,30 +103,39 @@ class QMS(tk.Canvas):
 
 
 master = tk.Tk()
-
+"""
+레이아웃 생성
+"""
 frame1 = tk.Frame(master, bg='#000')
 frame2 = tk.Frame(master, bg='#000')
 frame1.pack(side='left')
 frame2.pack()
-
+"""
+클래스 호출
+"""
 lab = QMS(frame1, width=500, height=150)
-
+"""
+라벨 생성 (그리드 사용)
+"""
 _ = tk.Label(frame2, text='Freq')
 _.config(bg='#000', fg='#FFF')
 _.grid(row=0, column=0)
 _ = tk.Label(frame2, text='Volt')
 _.config(bg='#000', fg='#FFF')
 _.grid(row=0, column=1)
-
+"""
+슬라이드바 생성
+"""
 f = tk.Scale(frame2, from_=0, to=100)
 f.config(relief='groove', bg='#000', fg='#FFF')
 f.grid(row=1, column=0)
 v = tk.Scale(frame2, from_=0, to=100)
 v.config(relief='groove', bg='#000', fg='#FFF')
 v.grid(row=1, column=1)
-lab.attachs(f, v)
-
-
+lab.attachs(f, v)  # 클래스와 연결
+"""
+버튼 생성
+"""
 _ = tk.Button(frame2, text='voltage', command=lab.rod_on)
 _.config(relief='groove', bg='#000', fg='#FFF')
 _.grid(row=2, column=0, columnspan=2, sticky='we')
