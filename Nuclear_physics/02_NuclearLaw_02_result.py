2b531d5eaaab435639ff097d580808cba0bf78cd
DESKTOP-R0ERG1T\soma0sd <soma0sd@gmail.com>
2016-12-03 19:45:48 +0900

    no message

---
+++ Nuclear_physics/02_NuclearLaw_02_result.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 19:25:18 2016
+@author: soma0sd
+"""
+
---
+++ Nuclear_physics/03_BatemanEq_01_result.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 19:25:00 2016
+@author: soma0sd
+"""
+
